57a258c7abd88b084eb2fc499fb1877181132669
Sigurd Hagen Falk <sigurd.falk@gmail.com>
2020-03-09 19:19:04 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstate71ac2932"
+    container_name       = "tfstate"
+    key                  = "platform.falkeredet.tfstate"
+  }
+}
+
+provider "azurerm" {
+  version = "=2.0.0"
+
+  features {}
+}
+
+resource "azurerm_resource_group" "platform_rg" {
+  name     = "rg-platform"
+  location = "West Europe"
+}
