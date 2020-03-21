256cc8aef924df58afef4288513181b63453313d
Sigurd Hagen Falk <sigurd.falk@gmail.com>
2020-03-22 00:42:38 +0100

    acr kv

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "kv_sdk_auth" {
-  value = module.kv_service_principal.sdk_auth
+  value       = module.kv_service_principal.sdk_auth
   description = "Output JSON compatible with the Azure SDK auth file."
 }
